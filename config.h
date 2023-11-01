c13aad9aac3e71d2771b07e5e703dadd1ac6794d
sayanta <sayantasingh01@gmail.com>
2023-11-02 04:26:14 +0530

    update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF Light:pixelsize=19:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 10;
 
